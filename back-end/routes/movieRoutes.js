96547a8dac0e4d701e571c6cdac461ffa860e66e
Purple Wisteria <plewista@gmail.com>
2021-08-11 12:50:43 +0530

    updating-netflix-clone-with-entry-form

--- back-end/routes/movieRoutes.js
+++ back-end/routes/movieRoutes.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler')
-const bodyParser = require('body-parser');
 
 const ActionMovie = require('../models/actionMovie')
 const NetflixOriginal = require('../models/netflixOriginal');
 const TopRated = require('../models/topRated');
+const User = require('./../models/user')
 
 const movieRoutes = express.Router()
-movieRoutes.use(bodyParser.json());
+movieRoutes.use(express.json());
+
 
 // @desc Fetch All Action Movies
 // @route GET /api/action-movies
 movieRoutes.get('/api/action-movies', asyncHandler (async (request, response) => {
-     const actionMovies = await ActionMovie.find({})
+     const actionMovies = await ActionMovie.find()
      response.json(actionMovies);  // converting into JSON content type
 }) )
 
@@ -31,7 +32,7 @@ movieRoutes.get('/api/action-movies/detail/:id', asyncHandler( async (request, r
 // @route GET /api/netflix-originals
 
 movieRoutes.get('/api/netflix-originals', asyncHandler (async (request, response) => {
-    const netflixOriginals = await NetflixOriginal.find({})
+    const netflixOriginals = await NetflixOriginal.find()
     response.json(netflixOriginals);  // converting into JSON content type
 }) )
 
@@ -47,7 +48,7 @@ movieRoutes.get('/api/netflix-originals/detail/:id', asyncHandler( async (reques
 
 
 movieRoutes.get('/api/top-rated', asyncHandler (async (request, response) => {
-    const topRatedMovies = await TopRated.find({})
+    const topRatedMovies = await TopRated.find()
     response.json(topRatedMovies);  // converting into JSON content type
 }) )
 
@@ -69,26 +70,24 @@ movieRoutes.post('/api/action-movie', asyncHandler( async (request, response) =>
         release_date, adult, backdrop_path, media_type, type,
         rating, numReviews } = request.body;
 
-   console.log(original_language, original_title, poster_path,  overview,
-    release_date, adult, backdrop_path, media_type, type,
-    rating, numReviews );
-    const actionMovie = new ActionMovie(
-        {   
-            original_language: original_language,
-            original_title: original_title,
-            poster_path: poster_path,
-            overview: overview,
-            release_date: release_date,
-            adult: adult,
-            backdrop_path: backdrop_path,
-            media_type: media_type,
-            type: type,
-            rating: rating,
-            numReviews: numReviews
-        }
-    );
-    
-	await actionMovie.save( (error, result) => {
+        const users = await User.find()
+        const adminUser = users[0]._id
+
+    const actionMovie =     {   
+        user: adminUser,
+        original_language: original_language,
+        original_title: original_title,
+        poster_path: poster_path,
+        overview: overview,
+        release_date: release_date,
+        adult: adult,
+        backdrop_path: backdrop_path,
+        media_type: media_type,
+        type: type,
+        rating: rating,
+        numReviews: numReviews
+    }
+    await ActionMovie.create(actionMovie, (error, actionMovie) => {
         if (error)     {
             return response.json({
                 status: false,
@@ -99,12 +98,13 @@ movieRoutes.post('/api/action-movie', asyncHandler( async (request, response) =>
         return response.json({
             status: true,
             message: 'Movied inserted!',
-            result: result
+            result: actionMovie
         })
     });
 	 
- })
-)
+   })   
+
+ )
 
 
 module.exports =  movieRoutes;
--- front-end/src/components/auth/MovieEntry.js
+++ front-end/src/components/auth/MovieEntry.js
@@ -82,74 +82,91 @@ const MovieEntry = () => {
     }
 
     return (
-      <div>
-    <MovieEntryFormContainer>
-    <Form onSubmit = {handleSubmit}>
-        {submitted && valid ? <div>Success!</div> : null }
-  <Fieldset>
-    <Title>Add Movie</Title>
-    <Input type="text" value={movie.original_language} name="original_language"
-        onChange={handleOriginalLanguageInputChange} placeholder="Original Language" />
-        {submitted && !movie.original_language ? <span>Enter Original Language</span> : null }
+      <Container>
+        <MovieEntryFormContainer>
+            <Form onSubmit = {handleSubmit}>
+                {submitted && valid ? <div>Success!</div> : null }
+            <Fieldset>
+                <Title>Add Movie</Title>
+                <Input type="text" value={movie.original_language} name="original_language"
+                onChange={handleOriginalLanguageInputChange} placeholder="Original Language" />
+                {submitted && !movie.original_language ? <Span>Enter Original Language</Span> : null }
 
-    <Input type="text" value={movie.original_title} name="original_title" 
-        onChange={handleOriginalTitleInputChange} placeholder="Original Title" />
-        {submitted && !movie.original_title ? <span>Enter Original Title</span> : null }
+                <Input type="text" value={movie.original_title} name="original_title" 
+                onChange={handleOriginalTitleInputChange} placeholder="Original Title" />
+                {submitted && !movie.original_title ? <Span>Enter Original Title</Span> : null }
 
-    <Input type="text" value={movie.poster_path} name="poster_path" 
-        onChange={handlePosterPathInputChange} placeholder="Poster Path" />
-        {submitted && !movie.poster_path ? <span>Enter Poster Path</span> : null }
+                <Input type="text" value={movie.poster_path} name="poster_path" 
+                onChange={handlePosterPathInputChange} placeholder="Poster Path" />
+                {submitted && !movie.poster_path ? <Span>Enter Poster Path</Span> : null }
 
-    <TextArea value={movie.overview} name="overview" 
-        onChange={handleOverviewInputChange} placeholder="Overview"/>
-        {submitted && !movie.overview ? <span>Enter Overview</span> : null }
+                <TextArea value={movie.overview} name="overview" 
+                onChange={handleOverviewInputChange} placeholder="Overview"/>
+                {submitted && !movie.overview ? <Span>Enter Overview</Span> : null }
 
-    <Input type="text" value={movie.release_date} name="release_date" 
-        onChange={handleReleaseDateInputChange} placeholder="Release Date" />
-        {submitted && !movie.release_data ? <span>Enter Release Data</span> : null }
+                <Input type="text" value={movie.release_date} name="release_date" 
+                onChange={handleReleaseDateInputChange} placeholder="Release Date" />
+                {submitted && !movie.release_date ? <Span>Enter Release Date</Span> : null }
 
-    <Input type="text" value={movie.adult} name="adult" 
-        onChange={handleAdultInputChange} placeholder="Adult" />
-        {submitted && !movie.adult ? <span>Enter Adult</span> : null }
+                <Input type="text" value={movie.adult} name="adult" 
+                onChange={handleAdultInputChange} placeholder="Adult" />
+                {submitted && !movie.adult ? <Span>Enter Adult</Span> : null }
 
-    <Input type="text" value={movie.backdrop_path} name="backdrop_path" 
-        onChange={handleBackdropPathInputChange} placeholder="Backdrop Path" />
-        {submitted && !movie.backdrop_path ? <span>Enter Backdrop Path</span> : null }
+                <Input type="text" value={movie.backdrop_path} name="backdrop_path" 
+                onChange={handleBackdropPathInputChange} placeholder="Backdrop Path" />
+                {submitted && !movie.backdrop_path ? <Span>Enter Backdrop Path</Span> : null }
 
-    <Input type="text" value={movie.media_type} name="media_type" 
-        onChange={handleMediaTypeInputChange} placeholder="Media Type" />
-        {submitted && !movie.media_type ? <span>Enter Media Type</span> : null }
+                <Input type= "text" value={movie.media_type} name="media_type" 
+                onChange={handleMediaTypeInputChange} placeholder="Media Type" />
+                {submitted && !movie.media_type ? <Span>Enter Media Type</Span> : null }
 
-    <Input type="text" value={movie.type} name="type" 
-        onChange={handleTypeInputChange} placeholder="Type" />
-        {submitted && !movie.type ? <span>Enter Type</span> : null }
+                <Input type="text" value={movie.type} name="type" 
+                onChange={handleTypeInputChange} placeholder="Type" />
+                {submitted && !movie.type ? <Span>Enter Type</Span> : null }
 
-    <Input type="text" value={movie.rating} name="rating" 
-        onChange={ handleRatingInputChange} placeholder="Rating" />
-        {submitted && !movie.rating ? <span>Enter Rating</span> : null }
+                <Input type="text" value={movie.rating} name="rating" 
+                onChange={ handleRatingInputChange} placeholder="Rating" />
+                {submitted && !movie.rating ? <Span>Enter Rating</Span> : null }
 
-    <Input type="text" value={movie.numReviews} name="numReviews" 
-        onChange={handleNumReviewsInputChange} placeholder="Number of Reviews" />
-        {submitted && !movie.numReviews ? <span>Enter Number of Reviews</span> : null }
+                <Input type="text" value={movie.numReviews} name="numReviews" 
+                onChange={handleNumReviewsInputChange} placeholder="Number of Reviews" />
+                {submitted && !movie.numReviews ? <Span>Enter Number of Reviews</Span> : null }
     
-    <AddMovie onClick={handleSubmit}>ADD MOVIE</AddMovie> 
-  </Fieldset>
-  </Form>
-  </MovieEntryFormContainer>
-
-        </div>
+                <AddMovie onClick={handleSubmit}>ADD MOVIE</AddMovie> 
+            </Fieldset>
+        </Form>
+    </MovieEntryFormContainer>
+  </Container>
     )
 }
 
 export default MovieEntry
 
+const Container = styled.div`
+    position: fixed;
+    z-index: -1;
+    height: 100%;
+    background-position:top;  // https://developer.mozilla.org/en-US/docs/Web/CSS/background-position
+    background-size: cover;   // Scales the image as large as possible to fill the container, stretching the image if necessary.
+    background-repeat: no-repeat;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3)),
+            url("/images/login-background.jpg");
+    position: absolute;            // https://developer.mozilla.org/en-US/docs/Web/CSS/position
+                                   //  It is positioned relative to its closest positioned ancestor                             
+    top:0px;
+    left: 0;
+    right:0;
+    z-index: -1;   // Giving low priority, anything can be added on top of BgImage 
+`;
+
 const MovieEntryFormContainer = styled.div`
-    width: 400px;
+    width: 40%;
     margin: 50px auto;
     text-align: center;
     position: relative;
     margin-top: 100px;
 `;
+
 
 const Form = styled.form`
 `;
@@ -158,11 +175,13 @@ const Fieldset = styled.div`
 background: white;
 border: 0 none;
 border-radius: 3px;
-box-shadow: 0 0 15px 1px rgba(0, 0, 0, 0.4);
+box-shadow: 5px 5px 5px 5px #525151;
 padding: 20px 30px;
 box-sizing: border-box;
 width: 80%;
 margin: 0 10%;
+display: flex;
+flex-direction: column;
 `;
 
 const Input = styled.input`
@@ -195,6 +214,7 @@ const AddMovie = styled.a`
     color: #f9f9f9;
     background-color: #0063e5;
     margin-bottom: 12px;
+    margin-top: 12px;
     width: 100%;
     letter-spacing: 1.5px;
     font-size: 18px;
@@ -213,4 +233,9 @@ font-size: 15px;
 text-transform: uppercase;
 color: #2C3E50;
 margin-bottom: 10px;
+`;
+
+ const Span = styled.span`
+ font-size: 12px;
+ color: #D10000;
 `;
